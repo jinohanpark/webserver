b3547b60e64d293323d66b09fd37e52f0c138b47
jinohan00 <jinohan00@naver.com>
2012-12-26 10:19:00 +0900

    a

---
+++ my_modules/socketio/socketio.js
@@ -0,0 +1,34 @@
+﻿
+var gcmyWsIO = new myWebSocket();
+exports = module.exports = gcmyWsIO;
+
+/*
+	internal modules
+*/
+var gmOS		= require('os');
+var gmUtil		= require('util');
+var gmFs		= require('fs');
+var gmBuffer	= require('buffer');
+
+/*
+	external modules
+*/
+
+/*
+	my modules
+*/
+
+
+/*
+PUBLIC function definition 
+*/
+function myWebSocket( _name )
+{
+	this.self = this;		// gcmyWsIO
+	
+}
+
+myWebSocket.prototype.createSocket = function()
+{
+
+}
